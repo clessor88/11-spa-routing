09f02febad47b7993f361fa315ec46d9381bf60a
Crystal Lessor <Crystal Lessor>
2016-06-20 20:20:56 -0700

    attempt to get spa-routing working

---
+++ starter-code/scripts/controllers/adminController.js
@@ -0,0 +1,12 @@
+
+(function(module) {
+  var adminController = {};
+
+  adminController.index = function() {
+    /* TODO: Reveal only the articles section! */
+    $('.tab-content').hide();
+    $('#blog-stats').fadeIn();
+  };
+
+  module.adminController = adminController;
+})(window);
